1c8f4654aded2686c3664e8959e8997ed8c5edf6
Nelson Matias <nellson.maia@gmail.com>
2025-04-23 20:06:40 +0100

    removing non used file

--- src/components/AuthWrapper.tsx
+++
@@ -1,7 +0,0 @@
-'use client';
-import { ReactNode } from 'react';
-import { UserProvider } from '@auth0/nextjs-auth0';
-
-export default function AuthWrapper({ children }: { children: ReactNode }) {
-  return <UserProvider>{children}</UserProvider>;
-}
