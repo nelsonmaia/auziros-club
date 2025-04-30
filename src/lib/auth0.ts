17d24adeaa2e128609f8da538310d1ff5ba3352b
Nelson Matias <nellson.maia@gmail.com>
2025-04-30 16:23:09 +0100

    chaning session time

--- src/lib/auth0.ts
+++ src/lib/auth0.ts
@@ -1,6 +1,10 @@
 import { Auth0Client } from '@auth0/nextjs-auth0/server';
 
 export const auth0 = new Auth0Client({
+  session: {
+    rolling: false, // session will not auto-extend on requests
+    absoluteDuration: 10, // session lasts 10 seconds total
+  },
   authorizationParameters: {
     audience: process.env.AUTH0_AUDIENCE,
     scope: process.env.AUTH0_SCOPE,
